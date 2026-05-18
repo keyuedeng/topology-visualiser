3ba6fec06921862ffb3271cf34f2edacbb4e3289
Karen Deng <keyuedeng6@gmail.com>
2026-05-19 09:49:24 +1000

    add graph builder

---
+++ graph.py
@@ -0,0 +1,10 @@
+import networkx as nx
+
+def build_graph(parsed_data, seed_hostname):
+    G = nx.Graph()
+    for entry in parsed_data:
+        device = entry['DEVICE_ID']
+        local_int = entry['LOCAL_INTERFACE']
+        remote_int = entry['REMOTE_INTERFACE']
+        G.add_edge(seed_hostname, device, local_interface=local_int, remote_interface=remote_int)
+    return G
